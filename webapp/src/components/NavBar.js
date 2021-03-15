ce22fc3c63fa07b315cfd14bf31bc3dbd09874e1
manuel-arroyo <UO264184@uniovi.es>
2021-03-16 00:15:32 +0100

    Cambios pre presentacion v.04

--- webapp/src/components/NavBar.js
+++ webapp/src/components/NavBar.js
@@ -2,10 +2,11 @@ import React from 'react';
 import {evaluateExpressions, useWebId} from '@solid/react';
 
 import BottomNav from './BottomNav';
+import Start from '../routes/StartPage';
 
 
 export default () => {
     //useWebId() te devuelve el url del perfil logeado con una String , que cambia automaticamente cuando el usuario se deslogea o cambia de perfil 
     const webId = useWebId()
-    return <BottomNav webId={webId} />;
+    return (webId) ? <BottomNav webId={webId} /> : <Start />;
 }
