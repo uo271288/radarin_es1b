dc69237aca0cd5c339316c83781b5b81f16308b3
uo269450 <UO269450@uniovi.es>
2021-03-16 11:02:35 +0100

    Update ButtonAddOrDelete.js

--- webapp/src/components/profile/ButtonAddOrDelete.js
+++ webapp/src/components/profile/ButtonAddOrDelete.js
@@ -1,66 +1,70 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-import { useLDflexList } from '@solid/react';
 
 import { Button } from '@material-ui/core';
 
 
+import solid from '@solid/query-ldflex';
+
+import { useLDflexValue, useLDflexList } from '@solid/react';
 
 
-function addFriend   ()  {
-        
+function addFriend() {
+
 }
 
-function removeFriend   ()  {
-    
+function removeFriend() {
+
 }
 
-/* async function getFriends(webId){
-    const friends= await solid.data[webId].friends;
-    return friends;
-}
- */
+async function checkFriend(loggedUser, webId, friends) {
+    console.log(friends)
+    for(const friendWebID of friends){
+        //console.log("Analysed: " +friendWebID +" and "+webId);
+        if(friendWebID==webId){
 
-async function checkFriend(loggedUser , webId, friends)  {
-
-    
-    console.log("Logged: " + loggedUser);
-    //console.log(typeof(friends))
-    console.log("Lista de amigos: ");
-    console.log(friends);  
-    
+            //console.log("Returned true: checkFriend")
+            return true;
+        }
+    }
 
 
-    console.log("------------")
+    //console.log("Returned false: checkFriend")
 
-    
     return false;
 }
 
-async function getFriends(friends){ 
-    const friendsValue=  await friends;
+async function getFriends(friends) {
+    //await new Promise(r => setTimeout(r, 2000));  
+    const friendsValue = await friends;
     return friendsValue;
 }
 
 
-export default  function ButtonAddOrDelete (props) {
+export default function ButtonAddOrDelete(props) {
+  
+
+    const [userFriendsList, setUserFriendsList] = useState([]);
+
+    const [webId] = useState(props.webId);
+    const [loggedUser] = useState(props.loggedUser);
 
 
-    const [webId] = useState( props.webId );
-    const [loggedUser] = useState( props.loggedUser );
+    const auxFriendsList = [];
 
+    getFriends(useLDflexList("[" + loggedUser + "].friends")).then((friendsList) => { setUserFriendsList(friendsList) });
     
-    const friends =   getFriends(useLDflexList("["+webId+"]"+'.friends') ).then((item)=> {return item;});
     
+    userFriendsList.forEach(element => auxFriendsList.push(element.valueOf()));
 
-    
-    return (
-        !checkFriend(loggedUser, webId, friends )?
-            <Button id="addFriend" onClick={addFriend}>Seguir</Button> 
-            : <Button id="remove Friend" onClick={removeFriend} >Dejar de seguir</Button>
+
+    return  (
+        ! checkFriend(loggedUser, webId, auxFriendsList) ?
+            (<Button id="addFriend" onClick={addFriend}>Seguir</Button>)
+            : (<Button id="remove Friend" onClick={removeFriend} >Dejar de seguir</Button>)
     )
 
-    
+
 }
 
 
