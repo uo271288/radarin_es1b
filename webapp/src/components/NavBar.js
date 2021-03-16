d4542b0f34cbd2a52023dd7a90b22eb16238cbb3
manuel-arroyo <UO264184@uniovi.es>
2021-03-16 01:04:14 +0100

    Prueba splcuion problemas ci

--- webapp/src/components/NavBar.js
+++ webapp/src/components/NavBar.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import {evaluateExpressions, useWebId} from '@solid/react';
-
+import {useWebId} from '@solid/react';
 import BottomNav from './BottomNav';
-import Start from '../routes/StartPage';
+import StartPage from '../routes/StartPage';
 
 
 export default () => {
     //useWebId() te devuelve el url del perfil logeado con una String , que cambia automaticamente cuando el usuario se deslogea o cambia de perfil 
     const webId = useWebId()
-    return (webId) ? <BottomNav webId={webId} /> : <Start />;
+    return (webId) ? <BottomNav webId={webId} /> : <StartPage />;
 }
