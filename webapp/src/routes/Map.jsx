8a5ea119ccff524ad77d6574461e098daf639e67
UO264184 <UO264184@uniovi.es>
2021-04-01 20:09:02 +0200

    Detección de estado y pais

--- webapp/src/routes/Map.jsx
+++ webapp/src/routes/Map.jsx
@@ -2,6 +2,14 @@ import React, { useEffect, useContext } from 'react'
 import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
 import { LocationsContext } from '../context/LocationsContext';
 import { addLocation, getFriendsLocations } from '../api/api';
+import Geocode from "react-geocode";
+
+
+Geocode.setApiKey("AIzaSyC6fKABMEcc3viILCEmzr9Uy7pToGhbVv0");
+Geocode.setLanguage("en");
+Geocode.setRegion("es");
+Geocode.setLocationType("ROOFTOP");
+Geocode.enableDebug();
 
 const Map = (props) => {
     const { locations, setLocations } = useContext(LocationsContext);
@@ -13,6 +21,7 @@ const Map = (props) => {
                 map.locate()
             },
             locationfound(e) {
+                saveLocation(e.latlng)
                 setPosition(e.latlng)
                 map.flyTo(e.latlng, map.getZoom())
                 console.log("Posición del user:"+ position);
@@ -32,6 +41,40 @@ const Map = (props) => {
         }else{
             return null;
         }
+    }
+
+    function saveLocation(latlng) {
+        
+        console.log(latlng)
+        Geocode.fromLatLng(latlng.lat, latlng.lng).then(
+            (response) => {
+              console.log(response)
+              const address = response.results[0].formatted_address;
+              let state, country;
+              for (let i = 0; i < response.results[0].address_components.length; i++) {
+                for (let j = 0; j < response.results[0].address_components[i].types.length; j++) {
+                  switch (response.results[0].address_components[i].types[j]) {
+                    case "administrative_area_level_1":
+                      state = response.results[0].address_components[i].long_name;
+                      break;
+                    case "country":
+                      country = response.results[0].address_components[i].long_name;
+                      break;
+                  }
+                }
+              }
+              console.log(state, country);
+              console.log(address);
+            },
+            (error) => {
+              console.error(error);
+            }
+        );
+    }
+
+    async function getFriends(friends){ 
+        const friendsValue=  await friends;
+        return friendsValue;
     }
 
     useEffect(() => {
