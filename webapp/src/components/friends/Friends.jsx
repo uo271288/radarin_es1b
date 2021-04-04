99f2e935fc3584f09de2022edfd8520ecba2f4fc
manuel-arroyo <UO264184@uniovi.es>
2021-04-04 19:33:13 +0200

    Añadido reac-swipeable-views para tabs

---
+++ webapp/src/components/friends/Friends.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import FriendsTabs from './FriendsTabs'
+import 
+
+const Friends = () => {
+    return (
+        <div>
+            <h1>Amigoos</h1>
+            <p>To be done: Vista para buscar usuarios</p>
+            <FriendsTabs /> 
+            <List></List>           
+        </div>
+    )
+}
+
+export default Friends
